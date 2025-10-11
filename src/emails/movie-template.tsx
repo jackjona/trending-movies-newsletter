68033988dce7c57fddef494bd121eea62eabcf42
Jack Jona <65679392+jackjona@users.noreply.github.com>
2025-10-11 05:57:24 +0000

    update dependencies

--- src/emails/movie-template.tsx
+++ src/emails/movie-template.tsx
@@ -87,7 +87,7 @@ export const MovieTemplate: React.FC<Readonly<MovieTemplateProps>> = ({
                 by TMDb.
               </Text>
               <Img
-                src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"
+                src="https://trending-movies.cyyz.workers.dev/tmbd.svg"
                 alt="TMDB Logo"
                 width="50px"
                 style={logoImage}
